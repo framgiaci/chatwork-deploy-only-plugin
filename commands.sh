c421ec8654460cc1e0a0ba0262a9803a8f24f397
dangminhtruong <xdangminhtruongx@gmail.com>
2018-10-18 14:40:47 +0700

    V1

--- commands.sh
+++ commands.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-curl -X POST -H "X-ChatWorkToken: $PLUGIN_TOKEN" -d \
-"body=[info][title]$PLUGIN_STATUS build $PLUGIN_BUILD_ID[/title]
-Repo: $PLUGIN_REPO
-Author: $PLUGIN_AUTHOR
-Branch: $PLUGIN_BRANCH
-Commit ID: $PLUGIN_COMMIT_ID
-Started at: $PLUGIN_CREATE_AT
-Build link: $PLUGIN_BUILD_LINK
-Commit messages: $PLUGIN_COMMIT_MESSAGE
-[/info]" \
-"https://api.chatwork.com/v2/rooms/$PLUGIN_ROOM_ID/messages"
+if [ ! -v ${PLUGIN_DEPLOY} ]; then
+    curl -X POST -H "X-ChatWorkToken: $PLUGIN_CHATWORK_TOKEN" -d \
+        "body=[info][title]$PLUGIN_BUILD_STATUS build $PLUGIN_BUILD_NUMBER[/title]
+        Repo: $PLUGIN_REPO_NAME
+        Author: $PLUGIN_COMMIT_AUTHOR
+        Branch: $PLUGIN_REPO_BRANCH
+        Auto deploy status: $PLUGIN_DEPLOY
+        Repo link: $PLUGIN_REPO_LINK
+        Commit messages: $PLUGIN_COMMIT_MESSAGE
+        [/info]" \
+        "https://api.chatwork.com/v2/rooms/$PLUGIN_ROOM_ID/messages" 
+fi
+
+exit 0
