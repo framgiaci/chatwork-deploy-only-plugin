27df6f16afc9be40187ba651569fac0dc00e7b08
Duong Quang Manh <duong.quang.manh@sun-asterisk.com>
2019-10-10 11:09:46 +0700

    change plugin_chatwork to chatwork

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -v ${PLUGIN_DEPLOY} ]; then
-    curl -X POST -H "X-ChatWorkToken: $PLUGIN_CHATWORK_TOKEN" -d \
+    curl -X POST -H "X-ChatWorkToken: $CHATWORK_TOKEN" -d \
         "body=[info][title]$PLUGIN_BUILD_STATUS build $PLUGIN_BUILD_NUMBER[/title]
         Repo: $PLUGIN_REPO_NAME
         Author: $PLUGIN_COMMIT_AUTHOR
@@ -9,7 +9,13 @@ if [ ! -v ${PLUGIN_DEPLOY} ]; then
         Repo link: $PLUGIN_REPO_LINK
         Commit messages: $PLUGIN_COMMIT_MESSAGE
         [/info]" \
-        "https://api.chatwork.com/v2/rooms/$PLUGIN_ROOM_ID/messages" 
+        "https://api.chatwork.com/v2/rooms/$ROOM_ID/messages" 
 fi
+echo "=======token=("
+echo $CHATWORK_TOKEN
+echo ")"
+echo "---room id=("
+echo $ROOM_ID 
+echo ")"
 
 exit 0
